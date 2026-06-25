9cb56bf10ee3bf5b85be694bfb9aace044438eab
Abe Kabakoff <occamsshavingkit@gmail.com>
2026-06-25 21:00:37 +0000

    T046: Add Arduino adapter skeleton

---
+++ platform/arduino/src/mu_arduino_adapter.h
@@ -0,0 +1,22 @@
+/* platform/arduino/src/mu_arduino_adapter.h */
+#ifndef MU_ARDUINO_ADAPTER_H
+#define MU_ARDUINO_ADAPTER_H
+
+#include "micro_opcua/platform.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Initialize the Arduino adapters */
+void mu_arduino_adapter_init(
+    mu_tcp_adapter_t *tcp_adapter,
+    mu_time_adapter_t *time_adapter,
+    mu_entropy_adapter_t *entropy_adapter
+);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MU_ARDUINO_ADAPTER_H */
