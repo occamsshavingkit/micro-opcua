ff1ffaa5ec91a6bf846d4f8b3a0e8d957bc05941
Abe Kabakoff <occamsshavingkit@gmail.com>
2026-06-25 21:41:58 +0000

    T111: Implement GetEndpoints response fields

--- tests/unit/test_discovery_services.c
+++ tests/unit/test_discovery_services.c
@@ -25,7 +25,27 @@ void test_discovery_findservers_response(void) {
 }
 
 void test_discovery_getendpoints_response(void) {
-    TEST_IGNORE_MESSAGE("Implement GetEndpoints response fields test");
+    mu_server_config_t config = {0};
+    config.application_uri = "urn:test:app";
+    config.product_uri = "urn:test:product";
+    config.application_name = "Test App";
+    config.endpoint_url = "opc.tcp://localhost:4840";
+    
+    mu_endpoint_description_t desc;
+    TEST_ASSERT_EQUAL(MU_STATUS_GOOD, mu_discovery_get_endpoint_description(&config, &desc));
+    
+    TEST_ASSERT_EQUAL_STRING("opc.tcp://localhost:4840", desc.endpoint_url);
+    TEST_ASSERT_EQUAL_STRING("urn:test:app", desc.server.application_uri);
+    
+    TEST_ASSERT_EQUAL(MU_MESSAGE_SECURITY_MODE_NONE, desc.security_mode);
+    TEST_ASSERT_EQUAL_STRING("http://opcfoundation.org/UA/SecurityPolicy#None", desc.security_policy_uri);
+    
+    TEST_ASSERT_EQUAL(1, desc.num_user_identity_tokens);
+    TEST_ASSERT_EQUAL_STRING("anonymous", desc.user_identity_tokens[0].policy_id);
+    TEST_ASSERT_EQUAL(MU_USER_TOKEN_TYPE_ANONYMOUS, desc.user_identity_tokens[0].token_type);
+    
+    TEST_ASSERT_EQUAL_STRING("http://opcfoundation.org/UA-Profile/Transport/uatcp-uasc-uabinary", desc.transport_profile_uri);
+    TEST_ASSERT_EQUAL(0, desc.security_level);
 }
 
 int main(void) {
