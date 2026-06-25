07a6fb0d42af45e472455a9aa9fae641e65de2ae
Abe Kabakoff <occamsshavingkit@gmail.com>
2026-06-25 20:46:16 +0000

    T008: Create shared Unity configuration

---
+++ tests/support/unity_config.h
@@ -0,0 +1,11 @@
+/* tests/support/unity_config.h */
+#ifndef UNITY_CONFIG_H
+#define UNITY_CONFIG_H
+
+/* Common Unity configuration for micro-opcua tests */
+
+/* Enable float and double support for scalar value tests */
+#define UNITY_INCLUDE_FLOAT
+#define UNITY_INCLUDE_DOUBLE
+
+#endif /* UNITY_CONFIG_H */
