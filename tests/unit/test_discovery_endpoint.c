1316fa5b724375abcec2b3ea547ed14aa503acb4
Abe Kabakoff <occamsshavingkit@gmail.com>
2026-06-25 21:37:26 +0000

    T105: Implement DiscoveryEndpoint dispatch

--- tests/unit/test_discovery_endpoint.c
+++ tests/unit/test_discovery_endpoint.c
@@ -5,12 +5,26 @@
 void setUp(void) {}
 void tearDown(void) {}
 
+#include "../../src/core/service_dispatch.h"
+
 void test_findservers_no_session(void) {
-    TEST_IGNORE_MESSAGE("Implement FindServers no-session test");
+    mu_server_t server;
+    opcua_byte_t req[10];
+    opcua_byte_t resp[10];
+    size_t resp_len = 10;
+    
+    opcua_statuscode_t status = mu_service_dispatch(&server, MU_ID_FINDSERVERSREQUEST, req, 10, resp, &resp_len);
+    TEST_ASSERT_EQUAL(MU_STATUS_GOOD, status);
 }
 
 void test_getendpoints_no_session(void) {
-    TEST_IGNORE_MESSAGE("Implement GetEndpoints no-session test");
+    mu_server_t server;
+    opcua_byte_t req[10];
+    opcua_byte_t resp[10];
+    size_t resp_len = 10;
+    
+    opcua_statuscode_t status = mu_service_dispatch(&server, MU_ID_GETENDPOINTSREQUEST, req, 10, resp, &resp_len);
+    TEST_ASSERT_EQUAL(MU_STATUS_GOOD, status);
 }
 
 int main(void) {
